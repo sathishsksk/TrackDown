bbe2840e63cec8ec5b795e69f578e716dd34e27f
Sathish Sk <67107485+sathishsksk@users.noreply.github.com>
2023-04-18 11:49:56 +0530

    Update sk.py

--- Sk/sk.py
+++ Sk/sk.py
@@ -1,17 +1,17 @@
 import base64
 from Crypto.Cipher import DES
-import binascii
 
 def decrypt_url(url):
     key = b"38346591"
-    iv = b"01234567"
+    iv = b"\0\0\0\0\0\0\0\0"
     cipher = DES.new(key, DES.MODE_CBC, iv)
-    encrypted_data = base64.b64decode(url)
-    decrypted_data = cipher.decrypt(encrypted_data)
-    padding_size = decrypted_data[-1]
-    decrypted_data = decrypted_data[:-padding_size]
-    return decrypted_data.decode('utf-8')
+    enc_url = base64.b64decode(url.strip())
+    dec_url = cipher.decrypt(enc_url)
+    dec_url = dec_url[:-dec_url[-1]]
+    dec_url = dec_url.decode('utf-8')
+    dec_url = dec_url.replace("_96.mp4", "_320.mp4")
+    return dec_url
 
-url = "ID2ieOjCrwfgWvL5sXl4B1ImC5QfbsDyFNsfQCO91T0dC9btMN5x/FlZmbA/MpEGc30F7gTMuUdelA8wZuh/oRw7tS9a8Gtq"
-decrypted_url = decrypt_url(url)
+encrypted_url = "ID2ieOjCrwfgWvL5sXl4B1ImC5QfbsDyFNsfQCO91T0dC9btMN5x/FlZmbA/MpEGc30F7gTMuUdelA8wZuh/oRw7tS9a8Gtq"
+decrypted_url = decrypt_url(encrypted_url)
 print(decrypted_url)
